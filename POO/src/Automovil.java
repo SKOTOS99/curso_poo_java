604b27b7228d7bfda809629ce181e6bca94d9096
SKOTOS99 <felipd95@hotmail.com>
2024-03-18 18:25:11 -0600

    init commit

---
+++ POO/src/Automovil.java
@@ -0,0 +1,129 @@
+
+public class Automovil {
+	
+	private String fabricante;
+	private String modelo;
+	private String color;
+	private int cantidadTanque = 40;
+	private double cilindrada;
+	
+	public  String detalle() {
+		return new StringBuilder("Fabricante: "+ this.fabricante)
+				.append(" \nModelo: "+ this.modelo)
+				.append("\nColor: " + this.color)
+				.append("\nCilindrada: "+ this.cilindrada)
+				.append("\nVelocidad: " + this.cilindrada * 100)
+				.toString();
+					
+	}
+	
+	public String acelerar(int rpm) {
+		return new StringBuilder("El auto: "+ this.fabricante)
+				.append(" acelerando a " + rpm)
+				.append(" rpm")
+				.toString();
+	}
+	
+	public String frenar() {
+		return new StringBuilder("El auto: "+ this.fabricante)
+				.append(" frenando " )
+				.toString();
+	}
+	
+	public String acelerarFrenar(int rpm) {
+		String acelerar = this.acelerar(rpm);
+		String frenar = this.frenar();
+		return acelerar + "\n" + frenar;
+		
+	}
+	
+	public float calcularConsumo(int km, float porcentajeGas) {
+		return (km/(porcentajeGas * this.cantidadTanque));
+	}
+	
+	//sobrecarga de metodo
+	public float calcularConsumo(int km, int porcentajeGas) {
+		return (km/((porcentajeGas)/100f* this.cantidadTanque));
+	}
+	
+	public String getFabricante() {
+		return fabricante;
+	}
+	public void setFabricante(String fabricante) {
+		this.fabricante = fabricante;
+	}
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public double getCilindrada() {
+		return cilindrada;
+	}
+	public void setCilindrada(double cilindrada) {
+		this.cilindrada = cilindrada;
+	}
+
+	public Automovil(String fabricante, String modelo, String color, int cantidadTanque, double cilindrada) {
+		this(fabricante, modelo, color, cantidadTanque);
+		this.cilindrada = cilindrada;
+	}
+
+	public Automovil() {
+		super();
+	}
+	
+	public Automovil(String fabricante, String modelo, String color) {
+		this.fabricante = fabricante;
+		this.modelo = modelo;
+		this.color = color;
+
+	}
+	
+	public Automovil(String fabricante, String modelo, String color, int cantidadTanque) {
+		this.fabricante = fabricante;
+		this.modelo = modelo;
+		this.color = color;
+		this.cantidadTanque = cantidadTanque;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if(!(obj instanceof Automovil)) {
+			return false;
+		}
+		Automovil a = (Automovil) obj;
+		
+		return (this.fabricante != null && this.modelo !=null && this.fabricante.equals(a.fabricante) 
+				&& this.modelo.equals(a.getModelo()));
+	}
+
+	@Override
+	public String toString() {
+		return "Automovil [fabricante=" + fabricante + ", modelo=" + modelo + ", color=" + color + ", cantidadTanque="
+				+ cantidadTanque + ", cilindrada=" + cilindrada + ", detalle()=" + detalle() + ", frenar()=" + frenar()
+				+ ", getFabricante()=" + getFabricante() + ", getModelo()=" + getModelo() + ", getColor()=" + getColor()
+				+ ", getCilindrada()=" + getCilindrada() + ", getClass()=" + getClass() + ", hashCode()=" + hashCode()
+				+ ", toString()=" + super.toString() + "]";
+	}
+	
+	
+	
+	
+	
+	
+
+
+
+	
+	
+	
+
+}
---
+++ POO/src/EjemploAutomovil.java
@@ -0,0 +1,49 @@
+import java.util.Date;
+
+public class EjemploAutomovil {
+
+	public static void main(String[] args) {
+		
+		Automovil auto = new Automovil();
+		 auto.setFabricante("Nisan");
+		 auto.setColor("rojo");
+		 auto.setCilindrada(250);
+		 auto.setModelo("20014");
+		 
+		System.out.println( auto.detalle());
+		System.out.println(auto.acelerar(6000));
+		System.out.println(auto.frenar());
+		System.out.println(auto.acelerarFrenar(7999));
+		
+		System.out.println("Kilometros por litro: "+ auto.calcularConsumo(5, 0.56f));
+		System.out.println("Kilometros por litro: "+ auto.calcularConsumo(5, 56));
+		
+		Automovil auto2 = new Automovil();
+		 auto2.setFabricante("Nisan");
+		 auto2.setColor("rojo");
+		 auto2.setCilindrada(250);
+		 auto2.setModelo("20014");
+		 
+		System.out.println( auto2.detalle());
+		System.out.println(auto2.acelerar(6000));
+		System.out.println(auto2.frenar());
+		System.out.println(auto2.acelerarFrenar(7999));
+		
+		System.out.println("Kilometros por litro: "+ auto2.calcularConsumo(5, 0.56f));
+		System.out.println("Kilometros por litro: "+ auto2.calcularConsumo(5, 56));
+		
+		
+		System.out.println(auto.equals(auto2));
+		
+		Automovil auto3 = new Automovil();
+		System.out.println(auto.equals(auto3));
+		System.out.println(auto.equals(new Date()));
+		
+		System.out.println(auto.toString());
+
+
+
+	}
+
+}
+ 
