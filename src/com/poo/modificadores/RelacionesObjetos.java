324e83fec7e3069994484d2e4d5ef4f03af39018
SKOTOS99 <felipd95@hotmail.com>
2024-03-21 17:37:49 -0600

    add constants and refactor array ruedas

--- src/com/poo/modificadores/RelacionesObjetos.java
+++ src/com/poo/modificadores/RelacionesObjetos.java
@@ -1,7 +1,7 @@
 package com.poo.modificadores;
-import java.util.Date;
 
 import com.poo.modificadores.constants.Colores;
+import com.poo.modificadores.constants.Constants;
 import com.poo.modificadores.constants.TipoAuto;
 import com.poo.modificadores.constants.TipoMotor;
 import com.poo.modificadores.models.automovil.Automovil;
@@ -10,34 +10,37 @@ import com.poo.modificadores.models.automovil.Persona;
 import com.poo.modificadores.models.automovil.Rueda;
 import com.poo.modificadores.models.automovil.Tanque;
 
-public class EjemploAutomovil {
+public class RelacionesObjetos {
 
 	public static void main(String[] args) {
-		
 		Automovil auto = new Automovil();
-		Rueda[] ruedas = {new Rueda("Micheline", 16, 24.5),
-				new Rueda("Micheline", 16, 24.5),
-				new Rueda("Micheline", 16, 24.5),
-				new Rueda("Micheline", 16, 24.5)};
+		Rueda[] ruedas = new Rueda[4];
+		for (int i = 0; i < ruedas.length; i++) {
+		    ruedas[i] = new Rueda(Constants.FABRICANTE_LLANTE, 16, 24.5);
+		}
 		Motor motor = new Motor(250, TipoMotor.MAGNA);
 		Tanque tanque = new Tanque(30);
 		Persona conductor = new Persona("Jesus", "Mendoza");
 		
 		 auto.setColor(Colores.AZUL);
-		 auto.setFabricante("yamaha");
+		 auto.setFabricante(Constants.YAMAHA);
 		 auto.setConductor(conductor);
 		 auto.setMotor(motor);
-		 //auto.setCantidadTanque(tanque);
+		 auto.setCantidadTanque(tanque);
 		 auto.setRuedas(ruedas);
 		 auto.setModelo("2024");
 		 auto.setTipo(TipoAuto.SEDAN);
 		 System.out.println(auto.calcularConsumo(12, 13));
 		 
 		 System.out.println(auto.detalle());
-
-
-
+		
 	}
+	
+	
+
+
+	
+
+
 
 }
- 
--- src/com/poo/modificadores/constants/Constants.java
+++ src/com/poo/modificadores/constants/Constants.java
@@ -3,5 +3,7 @@ package com.poo.modificadores.constants;
 public class Constants {
 	
 	public static final Integer FOURT_TEEN = 40;
+	public static final String FABRICANTE_LLANTE = "Micheline";
+	public static final String YAMAHA = "yamaha";
 
 }
